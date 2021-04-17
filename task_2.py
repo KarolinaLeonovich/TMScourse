e8c5acdb07be6f17e71be7d8165f496de67bafd8
KarolinaLeonovich <82409639+KarolinaLeonovich@users.noreply.github.com>
2021-04-17 13:33:11 +0300

    Add files via upload

---
+++ task_2.py
@@ -0,0 +1,11 @@
+string = "0123456789"
+slice_1 = string[2]
+print(slice_1)
+slice_2 = string[-2]
+print(slice_2)
+slice_3 = string[:5]
+print(slice_3)
+slice_4 = string[:-2]
+print(slice_4)
+slice_5 = string[::2]
+print(slice_5)
